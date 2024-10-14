5927cb7c09f91d0e00fc3feca4ddbebdff7cc9d1
Omz <78838816+omerdavidian@users.noreply.github.com>
2024-10-14 15:31:55 -0600

    update

--- Java Script/Small Code Challenges/groceries.js
+++ Java Script/Small Code Challenges/groceries.js
@@ -12,3 +12,33 @@ groceries( [{item: 'Bread'}, {item: 'Butter'}] );
 groceries( [{item: 'Cheese Balls'}] );
 // returns 'Cheese Balls'
 */
+listItems = [
+  { item: "Carrots" },
+  { item: "Hummus" },
+  { item: "Pesto" },
+  { item: "Rigatoni" },
+];
+
+const groceries = (arr) => {
+  let list = [];
+
+  arr.forEach((a) => {
+    list.push(a.item);
+  });
+
+  if (list.length === 1) {
+    console.log(list[0]);
+  } else {
+    let string = "";
+    for (let i = 0; i < list.length - 2; i++) {
+      string = string.concat(list[i] + ", ");
+    }
+    string = string.concat(
+      list[list.length - 2] + " and " + list[list.length - 1]
+    );
+
+    console.log(string);
+  }
+};
+
+groceries(listItems);
