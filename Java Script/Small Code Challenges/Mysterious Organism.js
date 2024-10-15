c85e56fee6aec57dcedfd6a22d7d368b3102d26e
Omz <78838816+omerdavidian@users.noreply.github.com>
2024-10-15 10:13:01 -0600

    step 3, 4

---
+++ Java Script/Small Code Challenges/Mysterious Organism.js
@@ -0,0 +1,25 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ["A", "T", "C", "G"];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single strand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+const pAequorFactory = (num, dnaArr) => {
+  return {
+    specimenNum: num,
+    dna: dnaArr,
+  };
+};
+
+const newOrganism = pAequorFactory(1, mockUpStrand());
+
+console.log(newOrganism);
