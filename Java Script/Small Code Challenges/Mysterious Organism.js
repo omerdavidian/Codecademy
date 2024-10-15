00824739cde5cc0dd24b702989f02013cfc8b635
Omz <78838816+omerdavidian@users.noreply.github.com>
2024-10-15 16:28:00 -0600

    step 7

--- Java Script/Small Code Challenges/Mysterious Organism.js
+++ Java Script/Small Code Challenges/Mysterious Organism.js
@@ -2,9 +2,7 @@
 const returnRandBase = (exclude = null) => {
   const dnaBases = ["A", "T", "C", "G"];
   //if exclude exist then return an array without excluded element. otherwise keep original array
-  const filteredBases = exclude
-    ? dnaBases.filter((base) => base !== exclude)
-    : dnaBases;
+  const filteredBases = exclude ? dnaBases.filter((base) => base !== exclude) : dnaBases;
   //filteredBases could be 4 element or 3 element if exclude exist.
   return filteredBases[Math.floor(Math.random() * filteredBases.length)];
 };
@@ -23,14 +21,14 @@ const pAequorFactory = (num, dnaArr) => {
     specimenNum: num,
     dna: dnaArr,
     mutate() {
-      console.log(dnaArr);
+      // console.log(dnaArr);
       const cellNum = Math.floor(Math.random() * 15);
-      console.log(`cell possition muteted: ${cellNum}`);
+      // console.log(`cell possition muteted: ${cellNum}`);
       const cellBase = dnaArr[cellNum];
-      console.log(`old dnaBase: ${cellBase}`);
+      // console.log(`old dnaBase: ${cellBase}`);
       //exclude the old cell dnaBase from the new random dnaBase
       dnaArr[cellNum] = returnRandBase(cellBase);
-      console.log(`new cell dana: ${dnaArr[cellNum]}`);
+      // console.log(`new cell dana: ${dnaArr[cellNum]}`);
       return dnaArr;
     },
     compareDNA(otherOrganism) {
@@ -44,21 +42,11 @@ const pAequorFactory = (num, dnaArr) => {
       console.log(`specimen #2 have ${similarity}% DNA in common`);
     },
     willLikelySurvive() {
-      let mostlyC = 0;
-      let mostlyG = 0;
-      for (let i = 0; i < this.dna.length; i++) {
-        if (this.dna[i] === "C") {
-          mostlyC++;
-        } else if (this.dna[i] === "G") {
-          mostlyG++;
-        }
-      }
-      console.log(`C count: ${mostlyC}`);
-      console.log(`G count: ${mostlyG}`);
-
-      const percentageC = (mostlyC / this.dna.length) * 100;
-      const percentageG = (mostlyG / this.dna.length) * 100;
-      return percentageC >= 60 || percentageG >= 60;
+      const survivalBases = this.dna.filter(
+        (base) => base === "C" || base === "G"
+      );
+      const survivalPercentage = (survivalBases.length / this.dna.length) * 100;
+      return survivalPercentage >= 60;
     },
   };
 };
@@ -67,7 +55,28 @@ const newOrganism1 = pAequorFactory(1, mockUpStrand());
 const newOrganism2 = pAequorFactory(2, mockUpStrand());
 // console.log(newOrganism1.dna);
 // console.log(newOrganism2.dna);
-newOrganism1.compareDNA(newOrganism2);
+// newOrganism1.compareDNA(newOrganism2);
 
-const mostlyC = newOrganism1.willLikelySurvive();
-console.log(mostlyC);
+// const mostlyC = newOrganism1.willLikelySurvive();
+// console.log(mostlyC);
+
+const instances = () => {
+  let specimenNum = 1;
+  const instanceArray = [];
+  while (instanceArray.length < 30) {
+    let checkSpecimen = pAequorFactory(specimenNum, mockUpStrand());
+    if (checkSpecimen.willLikelySurvive() === true) {
+      instanceArray.push({
+        specimenNum: specimenNum,
+        dna: checkSpecimen.dna.join(" "),
+      });
+    }
+
+    specimenNum++;
+  }
+  return {
+    totalSpecimens: instanceArray.length,
+    instances: instanceArray,
+  };
+};
+console.log(instances());
