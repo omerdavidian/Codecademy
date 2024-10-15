5e75398f4f48e9f1a165e0fed424802bb0746022
Omz <78838816+omerdavidian@users.noreply.github.com>
2024-10-15 10:47:03 -0600

    step 4

--- Java Script/Small Code Challenges/Mysterious Organism.js
+++ Java Script/Small Code Challenges/Mysterious Organism.js
@@ -1,7 +1,12 @@
 // Returns a random DNA base
-const returnRandBase = () => {
+const returnRandBase = (exclude = null) => {
   const dnaBases = ["A", "T", "C", "G"];
-  return dnaBases[Math.floor(Math.random() * 4)];
+  //if exclude exist then return an array without excluded element. otherwise keep original array
+  const filteredBases = exclude
+    ? dnaBases.filter((base) => base !== exclude)
+    : dnaBases;
+  //filteredBases could be 4 element or 3 element if exclude exist.
+  return filteredBases[Math.floor(Math.random() * filteredBases.length)];
 };
 
 // Returns a random single strand of DNA containing 15 bases
@@ -17,9 +22,20 @@ const pAequorFactory = (num, dnaArr) => {
   return {
     specimenNum: num,
     dna: dnaArr,
+    mutate() {
+      console.log(dnaArr);
+      const cellNum = Math.floor(Math.random() * 15);
+      console.log(`cell possition muteted: ${cellNum}`);
+      const cellBase = dnaArr[cellNum];
+      console.log(`old dnaBase: ${cellBase}`);
+      //exclude the old cell dnaBase from the new random dnaBase
+      dnaArr[cellNum] = returnRandBase(cellBase);
+      console.log(`new cell dana: ${dnaArr[cellNum]}`);
+      return dnaArr;
+    },
   };
 };
 
 const newOrganism = pAequorFactory(1, mockUpStrand());
 
-console.log(newOrganism);
+console.log(newOrganism.mutate());
